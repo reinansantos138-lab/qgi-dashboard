c21705e9b0eaf4eaacbef5ea1d3ac90c5daf1b34
reinansantos138-lab <reinansantos138@gmail.com>
2026-01-16 23:02:08 -0300

    Versão inicial PWA

---
+++ LabQGI/service-worker.js
@@ -0,0 +1,46 @@
+const CACHE_NAME = 'qgi-lab-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://unpkg.com/react@18/umd/react.development.js',
+  'https://unpkg.com/react-dom@18/umd/react-dom.development.js',
+  'https://unpkg.com/@babel/standalone/babel.min.js',
+  'https://cdn.tailwindcss.com'
+];
+
+// Instalação do Service Worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Cache aberto');
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+// Interceptação de requisições para servir cache offline
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      // Retorna do cache se existir, senão busca na rede
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// Atualização do Service Worker (limpeza de caches antigos)
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
